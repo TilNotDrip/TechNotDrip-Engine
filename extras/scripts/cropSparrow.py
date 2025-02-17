cc90d8d34b17a2758e2d78fdba4abdc09b4f3dc5
Til Tjardes <til.tjardes@gmail.com>
2025-02-16 19:16:17 -0700

    cropping sparrow images/optimizing them

---
+++ extras/scripts/cropSparrow.py
@@ -0,0 +1,25 @@
+import os
+from xml.dom import minidom
+from PIL import Image
+
+for (dirpath, dirnames, filenames) in os.walk('../..'):
+	for file in filenames:
+		if file.endswith(".xml") and os.path.isfile(dirpath + '/' + file[:-3] + 'png'):
+			print('Converting ' + file[:-4])
+
+			xmlParse = minidom.parse(dirpath + '/' + file)
+			
+			highestWidth = 0
+			highestHeight = 0
+			for elem in xmlParse.getElementsByTagName('SubTexture'):
+				width = int(elem.attributes['x'].value) + int(elem.attributes['width'].value)
+				height = int(elem.attributes['y'].value) + int(elem.attributes['height'].value)
+
+				if width > highestWidth:
+					highestWidth = width
+				if height > highestHeight:
+					highestHeight = height
+
+			image = Image.open(dirpath + '/' + file[:-3] + 'png')
+			image.crop((0, 0, highestWidth, highestHeight))
+			image.save(dirpath + '/' + file[:-3] + 'png')
