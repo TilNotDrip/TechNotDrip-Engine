bcbeabef1744cc44d1faca380ef89e47f2b5674e
Til <89487150+TechnikTil@users.noreply.github.com>
2025-03-22 19:55:40 -0600

    fix MenuState items being weirdly offset

--- src/funkin/states/ui/MenuState.hx
+++ src/funkin/states/ui/MenuState.hx
@@ -51,7 +51,7 @@ class MenuState extends FunkinState
 	{
 		if (FlxG.sound.music == null)
 		{
-			conductor.bpm = 102;
+			conductor.changeBPM(102);
 			FlxG.sound.playMusic(Paths.content.audio('ui/menu/freakyMenu'));
 		}
 
@@ -186,6 +186,7 @@ class MenuState extends FunkinState
 			itemSpr.addAnimation('selected', item.id + ' selected', [], 30, true);
 			itemSpr.playAnimation('idle');
 			itemSpr.updateHitbox();
+			itemSpr.screenCenter(X);
 			menuItemGroup.add(itemSpr);
 		}
 	}
@@ -212,9 +213,7 @@ class MenuState extends FunkinState
 			else
 				item.animation.play('idle', true);
 
-			item.updateHitbox();
-			item.centerOrigin();
-			item.screenCenter(X);
+			item.centerOffsets();
 		});
 
 		#if FUNKIN_DISCORD_RPC
