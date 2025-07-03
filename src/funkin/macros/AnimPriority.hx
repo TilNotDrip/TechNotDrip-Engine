5727048ac4ae8cb655e14512debd8296d9a35110
CrusherNotDrip <crushernotdrip@tilnotdrip.org>
2025-07-02 20:47:07 -0400

    Animation Priority Macro

---
+++ src/funkin/macros/AnimPriority.hx
@@ -0,0 +1,31 @@
+package funkin.macros;
+
+#if !display
+#if macro
+import haxe.macro.Context;
+import haxe.macro.Expr;
+
+@:dox(hide)
+class AnimPriority
+{
+	/**
+	 * Builds the field for the `priority` property.
+	 * @return New `priority` field.
+	 */
+	public static macro function build():Array<Field>
+	{
+		var fields:Array<Field> = Context.getBuildFields();
+
+		fields.push({
+			name: 'priority',
+			doc: 'The current priority that this animation has over upcoming animations.',
+			access: [Access.APublic],
+			kind: FieldType.FVar(macro :Int, macro $v{0}),
+			pos: Context.currentPos(),
+		});
+
+		return fields;
+	}
+}
+#end
+#end
