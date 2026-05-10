460bf69ae0253394bf5360113034c9bcd4a2ad59
CrusherNotDrip <crushernotdrip@tilnotdrip.org>
2026-05-10 14:29:49 -0400

    shoutout linus my biggest inspiration and steam
    Signed-off-by: CrusherNotDrip <crushernotdrip@tilnotdrip.org>

--- src/funkin/util/macro/AnimPriority.hx
+++ src/funkin/util/macro/AnimPriority.hx
@@ -1,4 +1,4 @@
-package funkin.macros;
+package funkin.util.macro;
 
 #if !display
 #if macro
@@ -8,24 +8,24 @@ import haxe.macro.Expr;
 @:dox(hide)
 class AnimPriority
 {
-	/**
-	 * Builds the field for the `priority` property.
-	 * @return New `priority` field.
-	 */
-	public static macro function build():Array<Field>
-	{
-		var fields:Array<Field> = Context.getBuildFields();
+  /**
+   * Builds the field for the `priority` property.
+   * @return New `priority` field.
+   */
+  public static macro function build():Array<Field>
+  {
+    var fields:Array<Field> = Context.getBuildFields();
 
-		fields.push({
-			name: 'priority',
-			doc: 'The current priority that this animation has over upcoming animations.',
-			access: [Access.APublic],
-			kind: FieldType.FVar(macro :Int, macro $v{0}),
-			pos: Context.currentPos(),
-		});
+    fields.push({
+      name: 'priority',
+      doc: 'The current priority that this animation has over upcoming animations.',
+      access: [Access.APublic],
+      kind: FieldType.FVar(macro :Int, macro $v{0}),
+      pos: Context.currentPos(),
+    });
 
-		return fields;
-	}
+    return fields;
+  }
 }
 #end
 #end
